2ac4c4f5e46160e138c8af8c2db66fc2fe1358c6
DiloenKarsten <svejrum@gmail.com>
2020-10-21 13:46:26 +0200

    first commit

---
+++ hello_pkg/scr/Variable.cpp
@@ -0,0 +1,23 @@
+// Variables are containers where you can store pieces 
+// of information. This will be shown in a short program.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+string animal = " monkey ";    
+string food = " sea banana ";
+int amount = 1;
+
+cout << "Deep under the sea a ferocious monster sea "  <<  animal  <<  " reigned" << endl;
+cout << "he was searching for "  << amount  <<  food << endl;
+cout << "therefore he wrecked havoc in the sea" << endl;
+cout << "he finally thought that he found the "  << food << endl;
+
+amount = -1;
+
+cout << "but it was only "  << amount  << food << endl;
+cout << "the "  << animal  << " went searching once more" << endl;
+
+}
